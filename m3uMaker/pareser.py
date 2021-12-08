9239ac2a7243e50afc595a029d0c3b1f43d2e160
gaoxiangyu <gaoxiangyu@jinninghui.com>
2021-12-08 16:25:27 +0800

    改造

--- m3uMaker/pareser.py
+++ m3uMaker/pareser.py
@@ -1,39 +1,39 @@
-import requests
-import random
-from time import sleep
+# -*- coding:utf-8 -*-
+import codecs
+import os
 from m3u_parser import M3uParser
 
-
-def sleep_random():
-    """随机等待5-10秒防止IP被封"""
-    sleep_s = random.randint(1, 10)
-    sleep(sleep_s)
-
-
-def check_url_ok(url):
-    """检测连接是否可用"""
-    useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
-    print("正在检查URL %s" % url)
-    try:
-        result = requests.get(url, headers={"User-Agent": useragent}, timeout=10)
-        return result.status_code == 200
-    except requests.exceptions.ConnectionError as e:
-        print("URL %s 访问超时" % url)
-        return False
-
-
-# URL 合集
-urls = [
-    "https://raw.fastgit.org/qwerttvv/Beijing-IPTV/master/IPTV-Unicom.m3u"
-]
-
+content = ''
+output = './output/checked.m3u'
 m3u_playlist = M3uParser()
 
-for url in urls:
-    m3u_playlist.parse_m3u(url)
-    m3u_list = map(lambda item: {"name": item.get("name", ""), "url": item.get("url", "")}, m3u_playlist.get_list())
-    for m in m3u_list:
-        sleep_random()
-        if not check_url_ok(m.get("url")):
-            continue
-        print("{name},{url}".format(name=m.get("name"), url=m.get("url", "")))
+
+def get_file_name(file_dir):
+    L = []
+    for root, dirs, files in os.walk(file_dir):
+        for file in files:
+            if os.path.splitext(file)[1] == '.m3u':
+                L.append(os.path.join(root, file))
+    return L
+
+
+def check(file_path):
+    global content
+    m3u_playlist.parse_m3u(file_path)
+    m3u_playlist.remove_by_extension("mp4")
+    m3u_playlist.filter_by("status", "GOOD")
+    print("check (%s), online size is [%d]" % (file_path, len(m3u_playlist.get_list())))
+
+
+if __name__ == '__main__':
+    if os.path.exists(output):
+        os.remove(output)
+        content = '#EXTM3U\n'
+    files = get_file_name('./input')
+    for file in files:
+        check(file)
+    for item in m3u_playlist.get_list():
+        content += "#EXTINF:-1," + item['name'] + '\n' + item['url'] + '\n'
+    f = codecs.open(output, 'a', 'utf-8')
+    f.write(content)
+    f.close()
